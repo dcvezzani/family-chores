06b82dbe01ef4a7c78b864f6b66967c88c3f52cd
David Vezzani <dcvezzani@churchofjesuschrist.org>
2021-02-20 20:24:01 -0700

    fixes redirect url

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,13 +2,40 @@ var express = require('express');
 var request = require('request');
 var router = express.Router();
 
+const getUser = (token) => {
+// GET https://graph.facebook.com/v9.0/me?fields=id%2Cname%2Cemail&access_token=EAABiMOZBbkfMBACxT2wNSMzpzMVykCSQNIsk9X3ogqxobxTwsDyZBpM6sOItKbMr0Ggba6yBVk3l1DKZBcbZBgWofloYzuhwYjsZBDzGlHQs5hwqSguDEnaHOHA5rzzt7ZBh8NIa6QuCkX9ZAbH9AHlsL7fy5qlYskyaHTC6wkfOAZDZD
+  
+  const options = {
+    method: 'get',
+    url: 'https://graph.facebook.com/v9.0/me',
+    qs: {
+      fields: 'id,name,email',
+      access_token: token,
+    },
+    json: true,
+  }
+  console.log(">>>options", options)
+
+  return new Promise((resolve, reject) => {
+    request(options, (error, response, body) => {
+      // console.log(">>>body", body)
+      if (!error && response.statusCode == 200) {
+        return resolve(body)
+      } else if (response.statusCode >= 400) {
+        return reject({message: `Unable to get token`, status: response.statusCode, error, body})
+      }
+    })
+  })
+  
+}
+
 const getToken = (code) => {
   const options = {
     method: 'get',
     url: 'https://graph.facebook.com/v9.0/oauth/access_token',
     qs: {
       client_id: '107962317902323',
-      redirect_uri: 'https://chores.vezzaniphotography.com/api/chores/token',
+      redirect_uri: 'https://chores.vezzaniphotography.com/login',
       client_secret: 'ffc294287fbe2192815f50cc76943871',
       code,
 
@@ -17,7 +44,6 @@ const getToken = (code) => {
     },
     json: true,
   }
-
   console.log(">>>options", options)
 
   return new Promise((resolve, reject) => {
@@ -33,7 +59,7 @@ const getToken = (code) => {
 }
 
 router.get('/authorize', function (req, res) {
-  var uri = `https://www.facebook.com/v9.0/dialog/oauth?client_id=107962317902323&redirect_uri=https://chores.vezzaniphotography.com/api/chores/token&state=xxt&scope=email`
+  var uri = `https://www.facebook.com/v9.0/dialog/oauth?client_id=107962317902323&redirect_uri=https://chores.vezzaniphotography.com/login&state=xxt&scope=email`
   console.log(">>>uri", uri)
   res.redirect(uri)
 })
@@ -43,6 +69,7 @@ router.get('/token', async function(req, res, next) {
   if (!code) return res.status(400).json({ message: `Missing authorization grant code` })
 
   const data = await getToken(code)
+  .then(data => getUser(data.access_token))
   .catch(err => console.error(`Unable to get token`, err))
 
   res.json({ data })
