82a37d621dcdfa68bd66a84f76f727b0edcadd0d
David Vezzani <dcvezzani@churchofjesuschrist.org>
2021-03-01 16:19:33 -0700

    moves cache so it doesn't trigger app reload in non-prod env

--- server/cache.js
+++ server/cache.js
@@ -1,13 +1,15 @@
 const fs = require('fs')
 
-const _users = JSON.parse(fs.readFileSync('./cache.json'))
+const CACHE_PATH = `./cache/cache.json`
+
+const _users = JSON.parse(fs.readFileSync(CACHE_PATH))
 
 exports.users = {
   all: _users,
   get: (id) => { return _users[id] },
   set: (id, user) => { 
     _users[id] = user
-    fs.writeFileSync('./cache.json', JSON.stringify(_users))
+    fs.writeFileSync(CACHE_PATH, JSON.stringify(_users))
   },
 }
 
